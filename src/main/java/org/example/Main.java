7b51dea0570ccb26dbfdad21b44c95977015a310
Nikita Ivanov <resay99@mail.ru>
2023-04-01 14:47:25 +0400

    new branch commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,11 +8,6 @@ public class Main {
         int bonus;
         System.out.println("Ваш счет: " + balance + " rub");
         System.out.println("Вы пополнили счет на: " + deposit + " rub");
-        
-        // Объявляете переменные для входных данных и
-        // параметров программы: начального счёта,
-        // суммы пополнения и тп
-
 
         if (deposit > 999) {
             bonus = deposit / 100;
@@ -22,11 +17,6 @@ public class Main {
         int account = deposit + balance + bonus;
         System.out.println("Ваш бонус: " + bonus + " rub");
         System.out.println("Итоговый счет: " + account + " rub");
-
-        // Условным оператором проверяете, превысила ли
-        // сумма пополнения порог, и для этих двух разных
-        // сценариев рассчитываете сумму бонуса и выводите
-        // на экран.
     }
 
 }
